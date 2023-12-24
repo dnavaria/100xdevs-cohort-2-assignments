d1d111a150f4ffeb18be65dec4fcc21afe870d50
dnavaria <dk17816@gmail.com>
2023-12-24 17:03:28 +0000

    completed middleware questions

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,7 +1,8 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
+
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
@@ -13,15 +14,34 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+function rateLimitter(req, res, next) {
+  const userId = req.headers["user-id"];
+  if (!userId) {
+    res.status(404).json({ msg: "User ID not found" });
+  }
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 0;
+  }
+  numberOfRequestsForUser[userId] += 1;
+  if (numberOfRequestsForUser[userId] > 5) {
+    res.status(404).json({ msg: "User not found" });
+  } else {
+    next();
+  }
+}
+
+app.use(rateLimitter);
+
+app.get("/user", function(req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function(req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 module.exports = app;
+
--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 
 const app = express();
 let errorCount = 0;
@@ -10,17 +10,33 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+app.get("/user", function (req, res) {
+  try {
+    throw new Error("User not found");
+    res.status(200).json({ name: "john" });
+  } catch {
+    errorCount += 1;
+    res.status(404).json({ msg: "User not found" });
+  }
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  try {
+    res.status(200).json({ msg: "created dummy user" });
+  } catch {
+    errorCount += 1;
+    res.status(404).json({ msg: "User not found" });
+  }
 });
 
-app.get('/errorCount', function(req, res) {
-  res.status(200).json({ errorCount });
+app.get("/errorCount", function (req, res) {
+  try {
+    res.status(200).json({ errorCount });
+  } catch {
+    errorCount += 1;
+    res.status(404).json({ msg: "User not found" });
+  }
 });
 
 module.exports = app;
+
