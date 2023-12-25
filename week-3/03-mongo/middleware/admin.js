13b62c22e244e0b5191e94b405408559ae2b20b6
dnavaria <dk17816@gmail.com>
2023-12-25 08:34:35 +0000

    added middleware

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,19 @@
+const Admin = require('../db').Admin;
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+    if (!username || !password) {
+        return res.status(400).send("Username or password missing in headers");
+    }
+    const queryResult = await Admin.findOne({ username: username, password: password });
+    if (!queryResult) {
+        return res.status(401).send("Unauthorized");
+    }
+    next();
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,17 @@
-function userMiddleware(req, res, next) {
+const User = require('../db').User;
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+    if (!username || !password) {
+        return res.status(400).send("Username or password missing in headers");
+    }
+    const queryResult = await User.findOne({ username: username, password: password });
+    if (!queryResult) {
+        return res.status(401).send("Unauthorized");
+    }
+    next();
 }
 
 module.exports = userMiddleware;
